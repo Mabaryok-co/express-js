5d1c91509ea5721e2767004804758f035ab3f550
Muhammad Rovino Sanjaya <rovino.sanjaya@primavista-solusi.com>
2025-02-13 14:56:46 +0700

    rewrite app.js, add helmet dependency

--- documentation/requirement.md
+++ documentation/requirement.md
@@ -14,3 +14,6 @@
 
 ### File Interceptor
 - TBA
+
+### Security
+- helmet
--- src/app.js
+++ src/app.js
@@ -1,25 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
-const xss = require('xss-clean');
-const mongoSanitize = require('express-mongo-sanitize');
 const compression = require('compression');
 const cors = require('cors');
-const passport = require('passport');
-const httpStatus = require('http-status');
-const config = require('./config/config');
-const morgan = require('./config/morgan');
-const { jwtStrategy } = require('./config/passport');
-const { authLimiter } = require('./middlewares/rateLimiter');
-const routes = require('./routes/v1');
-const { errorConverter, errorHandler } = require('./middlewares/error');
-const ApiError = require('./utils/ApiError');
+const routes = require('./routes/routes');
 
 const app = express();
-
-if (config.env !== 'test') {
-  app.use(morgan.successHandler);
-  app.use(morgan.errorHandler);
-}
 
 // set security HTTP headers
 app.use(helmet());
@@ -31,8 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // sanitize request data
-app.use(xss());
-app.use(mongoSanitize());
+// Sanitize xss
+// Sanitize sqlinjection
 
 // gzip compression
 app.use(compression());
@@ -41,27 +26,13 @@ app.use(compression());
 app.use(cors());
 app.options('*', cors());
 
-// jwt authentication
-app.use(passport.initialize());
-passport.use('jwt', jwtStrategy);
-
-// limit repeated failed requests to auth endpoints
-if (config.env === 'production') {
-  app.use('/v1/auth', authLimiter);
-}
-
-// v1 api routes
-app.use('/v1', routes);
+//api routes
+app.use('', routes);
 
 // send back a 404 error for any unknown api request
 app.use((req, res, next) => {
-  next(new ApiError(httpStatus.NOT_FOUND, 'Not found'));
+  next(new Error('Not Found'));
 });
 
-// convert error to ApiError, if needed
-app.use(errorConverter);
-
-// handle error
-app.use(errorHandler);
 
 module.exports = app;
